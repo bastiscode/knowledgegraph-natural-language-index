b10b6f7140c0d534fb605bee1f7129f027a99ace
Sebastian Walter <sebastian.walter98@gmail.com>
2023-07-26 20:30:10 +0200

    add missing ( in regex

--- src/lib.rs
+++ src/lib.rs
@@ -134,7 +134,7 @@ impl KnowledgeGraphProcessor {
         let ent_pattern = Regex::new(match kg {
             KnowledgeGraph::Wikidata => r"<?http://www.wikidata.org/entity/(Q\d+)>?",
             KnowledgeGraph::Freebase => r"<?http://rdf.freebase.com/ns/(m\.[^>]+)>?",
-            KnowledgeGraph::DBPedia => r"<?http://dbpedia.org/resource/[^>]+)>?",
+            KnowledgeGraph::DBPedia => r"<?http://dbpedia.org/resource/([^>]+)>?",
         })?;
 
         Ok(Self {
